a79318d63decf2be93e895b3a7aea604adb2f922
cubdesign <takeo@cubdesign.com>
2022-06-29 00:26:46 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      mono: [
+        "Menlo",
+        "Monaco",
+        "Lucida Console",
+        "Liberation Mono",
+        "DejaVu Sans Mono",
+        "Bitstream Vera Sans Mono",
+        "Courier New",
+        "monospace",
+      ],
+    },
+  },
+  plugins: [],
+};
